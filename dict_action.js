1a416f547acf1b6bcd279a37cde005dfbdd8904c
sarunyoo wangwong <jing@sarunyoos-MacBook-Pro.local>
2014-10-04 16:24:05 +0700

    catch error

--- dict_action.js
+++ dict_action.js
@@ -4,11 +4,11 @@ var no_meaning_template = "<b id='no_mean'>Not found !</b>"
 
 // custom selector
 $.expr[':'].text_match = function  (obj, index, meta, stack) {
-  var text = $(obj).text();
-  // argument of selector
-  var regexp = meta[meta.length-1];
-  var r = new RegExp("^"+regexp+"$", "i");
-  return r.test(text);
+    var text = $(obj).text();
+    // argument of selector
+    var regexp = meta[meta.length-1];
+    var r = new RegExp("^"+regexp+"$", "i");
+    return r.test(text);
 }
 
 function log_word_history_from_background(req, sender, sendResponse) {
@@ -51,10 +51,10 @@ function log_word_history(word, meanings, soundlist) {
     }
 
     var w = new Wordlist({
-            word: word.toLowerCase(),
-            meaning: candidate_meanings,
-            created_at: new Date()
-        }, function() {
+        word: word.toLowerCase(),
+        meaning: candidate_meanings,
+        created_at: new Date()
+    }, function() {
         this.save();
     });
 }
@@ -64,82 +64,82 @@ function longdo_lookup(word, cb, bf, last_char, do_log) {
     do_log = do_log || false;
 
     $.ajax({
-      url: api + word,
-      beforeSend: bf || function(){},
-      error: function  () {
-          cb({},"error");
-      },
-      success: function  (raw_html) {
-          var tb = $("<div>"+raw_html+"</div>").find("tr:has(a:text_match('"+word+"'))")
-          var meanings = [];
-          var soundlist = []; // { type: 'uk', src: '...' }
-          tb.each(function  () {
-            // assume TR having img tag is sound button img
-            var el = $(this).find("img");
-            if(el.length > 0 ){
-               // finding a contain voice in href
-               var v = $(this).find("a[href*='voice']");
-               if(v.length > 0){
-                   var link = v.attr("href");
-                   var matches = /voice=(.*)&/i.exec(link);
-                   // checking duplication type
-                   for(var i in soundlist){
-                        var s = soundlist[i]; 
-                        // voice duplicated
-                        if(s.type == matches[1]) return;
-                   }
-                   // add to list
-                   soundlist.push({
-                       type: matches[1],
-                       src: link
-                   });
-               }
-               // extract sound 
-               return;
-            }
-            var m = $(this).find("td:eq(1)").html()
-            // should contain 'See also' or 'Syn'
-            if(/see also|syn|ant|example/i.test(m)) 
-                meanings.push(m)
-          });
-          
-          if(meanings.length == 0){
-            // query again without filter
+        url: api + word,
+        beforeSend: bf || function(){},
+        error: function  () {
+            cb({},"error");
+        },
+        success: function  (raw_html) {
+            var tb = $("<div>"+raw_html+"</div>").find("tr:has(a:text_match('"+word+"'))")
+            var meanings = [];
+            var soundlist = []; // { type: 'uk', src: '...' }
             tb.each(function  () {
-              // skip if has any image tag
-              var el = $(this).find("img");
-              if(el.length > 0 ) return;
-              var m = $(this).find("td:eq(1)").html();
-              meanings.push(m);
+                // assume TR having img tag is sound button img
+                var el = $(this).find("img");
+                if(el.length > 0 ){
+                    // finding a contain voice in href
+                    var v = $(this).find("a[href*='voice']");
+                    if(v.length > 0){
+                        var link = v.attr("href");
+                        var matches = /voice=(.*)&/i.exec(link);
+                        // checking duplication type
+                        for(var i in soundlist){
+                            var s = soundlist[i]; 
+                            // voice duplicated
+                            if(s.type == matches[1]) return;
+                        }
+                        // add to list
+                        soundlist.push({
+                            type: matches[1],
+                            src: link
+                        });
+                    }
+                    // extract sound 
+                    return;
+                }
+                var m = $(this).find("td:eq(1)").html()
+                    // should contain 'See also' or 'Syn'
+                    if(/see also|syn|ant|example/i.test(m)) 
+                        meanings.push(m)
             });
-          }
-          
-          // check result and wisely search more
-          if(meanings.length > 0) {
-              // log history
-              if(do_log) log_word_history(word, meanings, soundlist);
-              // return to callback
-              cb(meanings, soundlist, word);
-          } else {
-            // if not found do
-            // check if last char is 's' try to search without it
-            // check if last chat is 'ed' do seach again
-            if(last_char && last_char == 'd' && /e$/.test(word)){
-              word = word.substring(0, word.length-1);
-              longdo_lookup(word, cb, null, 'e');
-            } else if(/s$/i.test(word)){
-              word = word.substring(0,word.length-1);
-              longdo_lookup(word, cb, null, 's')
-            } else if(/ed$/i.test(word)){
-              word = word.substring(0,word.length-1);
-              longdo_lookup(word, cb, null, 'd')
-            } else{
-              cb(["not found"], word);
+
+            if(meanings.length == 0){
+                // query again without filter
+                tb.each(function  () {
+                    // skip if has any image tag
+                    var el = $(this).find("img");
+                    if(el.length > 0 ) return;
+                    var m = $(this).find("td:eq(1)").html();
+                    meanings.push(m);
+                });
             }
 
-          }
-          // send array of meaning to callback
-          // cb(meanings ,word)
+            // check result and wisely search more
+            if(meanings.length > 0) {
+                // log history
+                if(do_log) log_word_history(word, meanings, soundlist);
+                // return to callback
+                cb(meanings, soundlist, word);
+            } else {
+                // if not found do
+                // check if last char is 's' try to search without it
+                // check if last chat is 'ed' do seach again
+                if(last_char && last_char == 'd' && /e$/.test(word)){
+                    word = word.substring(0, word.length-1);
+                    longdo_lookup(word, cb, null, 'e');
+                } else if(/s$/i.test(word)){
+                    word = word.substring(0,word.length-1);
+                    longdo_lookup(word, cb, null, 's')
+                } else if(/ed$/i.test(word)){
+                    word = word.substring(0,word.length-1);
+                    longdo_lookup(word, cb, null, 'd')
+                } else{
+                    cb(["not found"], word);
+                }
+
+            }
+            // send array of meaning to callback
+            // cb(meanings ,word)
         }
     });
 }
@@ -149,25 +149,27 @@ chrome.runtime.onMessage.addListener(function(r, sender, sendResponse) {
 });
 
 // adding listener for external message
-chrome.runtime.onMessageExternal.addListener(
-    function(req, res, sendResponse) {
-        // sending array of
-        // { 
-        //   meaning:
-        //   word:
-        // }
-        var data = [];
+try {
+    chrome.runtime.onMessageExternal.addListener(
+        function(req, res, sendResponse) {
+            // sending array of
+            // { 
+            //   meaning:
+            //   word:
+            // }
+            var data = [];
+            Wordlist.all(function(rs) {
+                for(var i = 0; i < rs.length; i++){
+                    data.push({ meaning: rs[i].meaning, word: rs[i].word});
+                }
+                sendResponse({ result: data });
+            });
 
-        Wordlist.all(function(rs) {
-            for(var i = 0; i < rs.length; i++){
-                data.push({ meaning: rs[i].meaning, word: rs[i].word});
-            }
-            sendResponse({ result: data });
-        });
-
-        // in order to able to send response
-        // after this listener timeout
-        return true;
-    }
-);
-
+            // in order to able to send response
+            // after this listener timeout
+            return true;
+        }
+    );
+} catch(err) {
+    // do nothing
+}
