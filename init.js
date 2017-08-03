8a2a2a839c782d1d0fa15bfa9f57ddbf6122ca69
jingz <wsaryoo@gmail.com>
2017-08-03 14:21:42 +0700

    start google tracking remove CSP errors

--- init.js
+++ init.js
@@ -77,3 +77,13 @@ $(document).ready(function  () {
     $("#param").focus();
 });
 
+// google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-103581133-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
